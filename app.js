1b41c4cc6656ce51589c9ec4d29b1794d2b02ec9
Alvin Lim <alim459@gmail.com>
2016-02-26 01:18:13 -0800

    some preliminary work

--- app.js
+++ app.js
@@ -1,5 +1,44 @@
+var mockMode = true;
 var app = angular.module('Brewmaster', ['ngMaterial']);
 
 app.controller('AppCtrl', function($scope) {
-  $scope.imagePath = 'img/washedout.png';
+	if (mockMode) {
+		$scope.beers = [
+		 	{
+			  	"name": "Thunderbird Lager",
+			  	"brewery": "UBC Brewery",
+			  	"type": "Lager",
+			  	"abv": "5.1%",
+			  	"ibu": "10 bitterness units",
+			  	"imageLocation": "images/stock-beer.jpg"
+		  	},
+		  	{	
+			  	"name": "Passive Aggressive",
+			  	"brewery": "Brassneck",
+			  	"type": "IPA",
+			  	"abv": "5.3%",
+			  	"ibu": "2 bitterness units",
+			  	"imageLocation": "images/ipa.jpg"
+		  	},
+		  	{
+			  	"name": "Southern Hop",
+			  	"brewery": "Main Street Brewery",
+			  	"type": "IPA",
+			  	"abv": "6.1%",
+			  	"ibu": "20 bitterness units",
+			  	"imageLocation": "images/stock-beer.jpg"
+			},
+			{
+			  	"name": "Sun God Wheat Ale",
+			  	"brewery": "R&B Brewery",
+			  	"type": "Hefeweizen",
+			  	"abv": "5.6%",
+			  	"ibu": "2 bitterness units",
+			  	"imageLocation": "images/TownHallHefeweizen.jpg"
+			}
+	  	]
+  	} else {
+  		//not mockmode beers
+  	}
+
 });
